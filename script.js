69cf0321c25a0847b8c7903b84092364e21bf41a
Avesh Khatik <avikhatik13@gmail.com>
2024-01-27 12:04:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,27 @@
+let msg = document.getElementById("message");
+let pass = document.getElementById("password");
+let str = document.getElementById("strength");
+
+
+pass.addEventListener("input", function(){
+    if(pass.value.length > 0){
+        msg.style.display = "block"
+    } else{
+        msg.style.display = "none"
+    }
+    if(pass.value.length < 4){
+        str.innerHTML = "weak";
+        pass.style.borderColor = "red"
+        msg.style.color = "red"
+    } 
+    else if(pass.value.length > 4 && pass.value.length < 8){
+        str.innerHTML  = "medium";
+        pass.style.borderColor = "yellow";
+        msg.style.color = "yellow"
+    }
+    else if(pass.value.length > 8){
+        str.innerHTML = "strong";
+        pass.style.borderColor = "green";
+        msg.style.color = "green"
+    }
+})
